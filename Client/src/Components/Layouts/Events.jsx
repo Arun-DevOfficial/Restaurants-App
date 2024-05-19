9302d4fb7678ce81918e1c043d711e44b66f4a86
Arun-DevOfficial <darunkumar1204@gmail.com>
2024-05-19 16:31:03 +0530

    update events.jsx

--- Client/src/Components/Layouts/Events.jsx
+++ Client/src/Components/Layouts/Events.jsx
@@ -1,4 +1,4 @@
-import Client from "../../assets/client.png";
+import Client from "../../assets/Client.png";
 import { FaRegStar, FaStar, FaStarHalf } from "react-icons/fa6";
 
 export default function Events() {

fd7523c912e8ae409b716fbcb9ff6626d6a894c7
Arun-DevOfficial <darunkumar1204@gmail.com>
2024-05-21 20:57:10 +0530

    Add Router Config

---
+++ Client/src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+const NotFound = () => {
+  return (
+    <div className="h-screen flex flex-col justify-center items-center bg-gray-100">
+      <img
+        src="https://freefrontend.com/assets/img/html-funny-404-pages/CodePen-404-Page.gif"
+        alt="Lost in Space Illustration"
+        className="min-h-screen w-screen"
+      />
+    </div>
+  );
+};
+
+export default NotFound;

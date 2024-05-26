ce12e54dae57898bcc3244b7ee09f51b1fbe1488
Arun-DevOfficial <darunkumar1204@gmail.com>
2024-05-26 20:17:14 +0530

    update links

--- Server/Controller/userAuth.js
+++ Server/Controller/userAuth.js
@@ -89,7 +89,7 @@ const handleLogin = async (req, res) => {
         httpOnly: true,
         secure: true,
         sameSite: true,
-        // domain: ".restaurants-app-aqfv.vercel.app",
+        domain: ".restaurants-app-green.vercel.app/",
       };
       res.cookie("jwt", token, cookieOptions);
       return res.status(200).json({ message: "User login successful." });
